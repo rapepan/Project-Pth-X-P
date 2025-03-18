33dd977aaf366a32d5cbe6ccd52cbfb39eee9c26
65160121 <65160121@go.buu.ac.th>
2025-03-19 03:00:41 +0700

    Ep.9

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const mysql = require("mysql2");
 
 // สร้างการเชื่อมต่อกับฐานข้อมูล MySQL
 const connection = mysql.createConnection({
-  host: "localhost", 
+  host: "10.104.22.34", 
   user: "root", // ชื่อผู้ใช้ MySQL
-  password: "1234", // รหัสผ่าน (ถ้าไม่มีให้เว้นว่าง)
+  password: "BPYodv23927", // รหัสผ่าน (ถ้าไม่มีให้เว้นว่าง)
   database: "pth-x-p", // ชื่อฐานข้อมูล
 });
 
